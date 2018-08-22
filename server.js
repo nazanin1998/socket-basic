e908d0327f20a4be92e3582d0f6c517904933b2a
nazanin fereydooni <na.fereydooni@yahoo.com>
2018-08-22 20:37:54 +0430

    second

---
+++ server.js
@@ -0,0 +1,11 @@
+var PORT = 3000;
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+
+app.use(express.static(__dirname + '/public'));
+
+http.listen(PORT, function(){
+  console.log('Server is started on port '+PORT);
+});
+ 
